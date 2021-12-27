15ab996008d17d0b983b28092acd3d9c8d1c456a
reyzonek <pawel1221@gmail.com>
2021-12-27 12:53:50 +0100

    Done

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,28 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  },
+  {
+    title: 'Book3',
+    author: 'Name3'
+  }
+]
 
+
+
+function getTheTitles() {
+  let result = [];
+  for (let i = 0; i < books.length; i++){   
+    //let next = books[i].title;
+    result.push(books[i].title);  
+  }
+  return result;
 };
-
+console.log(getTheTitles());
 // Do not edit below this line
-module.exports = getTheTitles;
+//module.exports = getTheTitles;
